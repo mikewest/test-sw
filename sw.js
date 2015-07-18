cc9729b370518e2de30e4e8fa089c422ea6e945a
Mike West <mike@mikewest.org>
2015-07-18 13:27:02 +0200

    test

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 self.onfetch = function(event) {
     console.dir(event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(fetch('http://www.w3.org/2008/site/images/logo-w3c-mobile-lg', { mode: 'no-cors' }));
 };
