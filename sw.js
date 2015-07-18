96021caf3a221b1901922688eac7e228eb93b75c
Mike West <mike@mikewest.org>
2015-07-18 13:17:14 +0200

    test

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('test-cache-1')
-      .then(function(cache) {
-        console.log('Opened cache');
-        console.dir(cache.addAll(['http://www.w3.org/2008/site/images/logo-w3c-mobile-lg']));
-      })
-  );
-});
-
 self.onfetch = function(event) {
     console.dir(event);
     event.respondWith(fetch(event.request));
